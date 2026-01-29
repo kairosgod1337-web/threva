54fa7f29866d92ba6195955d4304ba0073f2e4f5
kairosgod1337-web <kairosgod1337@gmail.com>
2026-01-29 11:30:54 +0200

    Add files via upload

---
+++ src/config/partners.ts
@@ -0,0 +1,20 @@
+import ahhaaEn from '@/assets/logos/ahhaa_eng.png';
+import ahhaaEt from '@/assets/logos/ahhaa_est.png';
+
+import vapianoEn from '@/assets/logos/Vapiano-logo.svg';
+import vapianoEt from '@/assets/logos/Vapiano-logo.svg';
+
+
+
+export const partners = [
+  {
+    en: ahhaaEn,
+    et: ahhaaEt,
+    alt: 'Ahhaa',
+  },
+  {
+    en: vapianoEn,
+    et: vapianoEt,
+    alt: 'Vapiano',
+  },
+];
